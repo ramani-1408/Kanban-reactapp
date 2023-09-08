f36f25e36af1540810144e5986bbfb99a64c1112
ramani_1408 <201220019@nitdelhi.ac.in>
2023-09-08 19:32:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './Components/Dropdown/style.css';
+import './Components/Card/style.css';
+import Dropdown from './Components/Dropdown/dropdown.js';
+import Card from './Components/Card/index.js';
+import { useState } from 'react';
 
 function App() {
+  const[selected,setSelected]=useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Dropdown selected={selected} setSelected={setSelected}/>
+    <Card/>
     </div>
   );
 }
